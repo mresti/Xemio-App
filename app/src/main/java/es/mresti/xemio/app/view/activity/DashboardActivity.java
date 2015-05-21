553f2db4f9287090e38295ddb614f69af27cad67
Esteban Dorado Roldan <fadoro@gmail.com>
2015-05-22 00:33:25 +0200

    Added sliding tab to dashboard activity

--- app/src/main/java/es/mresti/xemio/app/view/activity/DashboardActivity.java
+++ app/src/main/java/es/mresti/xemio/app/view/activity/DashboardActivity.java
@@ -4,19 +4,35 @@ package es.mresti.xemio.app.view.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import es.mresti.xemio.R;
 import es.mresti.xemio.app.navigation.Navigator;
+import es.mresti.xemio.app.view.adapter.ViewPagerAdapter;
+import es.mresti.xemio.app.view.widget.SlidingTabLayout;
 
 public class DashboardActivity extends BaseActivity {
+
+  String TITLES[] = {"Inicio","Historial","Incidencias"};
+  int ICONS[] = {R.drawable.ic_home,R.drawable.ic_history,R.drawable.ic_assignment};
 
   private Navigator mNavigator;
 
   @InjectView(R.id.toolbar)
   Toolbar mtoolbar;
+
+  // Declaring Your View and Variables
+  ViewPager pager;
+  ViewPagerAdapter adapter;
+  SlidingTabLayout tabs;
+  CharSequence Titles[]={"Inicio","Historial"};
+  int Numboftabs =2;
+
 
   public static Intent getCallingIntent(Context context) {
     return new Intent(context, DashboardActivity.class);
@@ -37,6 +53,49 @@ public class DashboardActivity extends BaseActivity {
   private void initialize() {
     this.mNavigator = new Navigator();
     setSupportActionBar(mtoolbar);
+
+    // Creating The ViewPagerAdapter and Passing Fragment Manager, Titles fot the Tabs and Number Of Tabs.
+    adapter =  new ViewPagerAdapter(getSupportFragmentManager(),Titles,Numboftabs);
+
+    // Assigning ViewPager View and setting the adapter
+    pager = (ViewPager) findViewById(R.id.pager);
+    pager.setAdapter(adapter);
+
+    // Assiging the Sliding Tab Layout View
+    tabs = (SlidingTabLayout) findViewById(R.id.tabs);
+    tabs.setDistributeEvenly(true); // To make the Tabs Fixed set this true, This makes the tabs Space Evenly in Available width
+
+    // Setting Custom Color for the Scroll bar indicator of the Tab View
+    tabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
+      @Override
+      public int getIndicatorColor(int position) {
+        return getResources().getColor(R.color.color_white);
+      }
+    });
+
+    // Setting the ViewPager For the SlidingTabsLayout
+    tabs.setViewPager(pager);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      return true;
+    }
+    return super.onOptionsItemSelected(item);
   }
 
 }
