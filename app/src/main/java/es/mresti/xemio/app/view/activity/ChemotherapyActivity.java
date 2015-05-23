ba31ea670919783e100984921eae50eba6e027c6
Esteban Dorado Roldan <fadoro@gmail.com>
2015-05-24 00:41:39 +0200

    Fixed chemotherapy view

--- app/src/main/java/es/mresti/xemio/app/view/activity/ChemotherapyActivity.java
+++ app/src/main/java/es/mresti/xemio/app/view/activity/ChemotherapyActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Spinner;
-import android.support.v7.widget.Toolbar;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
