dcf4824177f6b8847b58ca444a669fbdfe260dcb
Esteban Dorado Roldan <fadoro@gmail.com>
2015-08-16 12:58:52 +0200

    Added Pass validator and test

---
+++ app/src/main/java/es/mresti/xemio/app/view/validator/PassValidator.java
@@ -0,0 +1,41 @@
+package es.mresti.xemio.app.view.validator;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import java.util.regex.Pattern;
+
+/**
+ * A validator for {@link android.widget.EditText}.
+ */
+public class PassValidator implements TextWatcher {
+  /**
+   * Alphanumeric validation pattern.
+   */
+  public static final Pattern ALL_CHARACTER_PATTERN = Pattern.compile("((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\\W]).{6,20})");
+
+  private boolean mIsValid = false;
+
+  public boolean isValid() {
+    return mIsValid;
+  }
+
+  /**
+   * Validates if the given input is an all character string.
+   *
+   * @param text The text to validate.
+   * @return {@code true} if the input is an all character string. {@code false} otherwise.
+   */
+  public static boolean isValidText(CharSequence text) {
+    return text != null && ALL_CHARACTER_PATTERN.matcher(text).matches();
+  }
+
+  @Override final public void afterTextChanged(Editable editableText) {
+    mIsValid = isValidText(editableText);
+  }
+
+  @Override final public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+  }
+
+  @Override final public void onTextChanged(CharSequence s, int start, int before, int count) {
+  }
+}
---
+++ app/src/test/java/es/mresti/xemio/app/view/validator/PassValidatorTest.java
@@ -0,0 +1,78 @@
+package es.mresti.xemio.app.view.validator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class PassValidatorTest {
+  @Test
+  public void characterValidator_CorrectAlphaNumeric(){
+    assertTrue(PassValidator.isValidText("Hello world 2015"));
+  }
+
+  @Test
+  public void characterValidator_CorrectAllCharacters(){
+    assertTrue(PassValidator.isValidText("Hello world, 2015!!"));
+  }
+
+  @Test
+  public void characterValidator_CorrectAllCharacters_Pass_1(){
+    assertTrue(PassValidator.isValidText("asdASDn12!"));
+  }
+
+  @Test
+  public void characterValidator_CorrectAllCharacters_Pass_2(){
+    assertTrue(PassValidator.isValidText("asdASDn12$"));
+  }
+
+  @Test
+  public void characterValidator_CorrectAllCharacters_Pass_3(){
+    assertTrue(PassValidator.isValidText("asdASDn12$%&"));
+  }
+
+  @Test
+  public void characterValidator_InvalidAlpha(){
+    assertFalse(PassValidator.isValidText("Helloworld"));
+  }
+
+  @Test
+  public void characterValidator_InvalidNumeric(){
+    assertFalse(PassValidator.isValidText("123456"));
+  }
+
+  @Test
+  public void characterValidator_InvalidSymbol(){
+    assertFalse(PassValidator.isValidText("!#$%&/"));
+  }
+
+  @Test
+  public void characterValidator_InvalidTooShort(){
+    assertFalse(PassValidator.isValidText("12aA@"));
+  }
+
+  @Test
+  public void characterValidator_InvalidRequiredUppercaseCharacter(){
+    assertFalse(PassValidator.isValidText("1234ab@"));
+  }
+
+  @Test
+  public void characterValidator_InvalidRequiredLowercaseCharacter(){
+    assertFalse(PassValidator.isValidText("1234AB@"));
+  }
+
+  @Test
+  public void characterValidator_InvalidRequiredSymbol(){
+    assertFalse(PassValidator.isValidText("1234abA"));
+  }
+
+  @Test
+  public void characterValidator_EmptyString(){
+    assertFalse(PassValidator.isValidText(""));
+  }
+
+  @Test
+  public void characterValidator_NullNumeric(){
+    assertFalse(PassValidator.isValidText(null));
+  }
+}
