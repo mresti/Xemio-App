52e7edbbd2cd36fee20f8c67867312f271709a3b
Esteban Dorado Roldan <fadoro@gmail.com>
2015-08-19 00:56:58 +0200

    Added MainView

---
+++ app/src/main/java/es/mresti/xemio/app/view/MainView.java
@@ -0,0 +1,13 @@
+package es.mresti.xemio.app.view;
+
+public interface MainView extends BaseView {
+  void showProgress();
+
+  void hideProgress();
+
+  void navigateToDashboardScreen();
+
+  void navigateToVerifyScreen();
+
+  void navigateToInitScreen();
+}
