82e7a66b2e5b3d6120f2d595330fb251d37462be
Esteban Dorado Roldan <fadoro@gmail.com>
2015-08-18 19:45:11 +0200

    Added History UI

---
+++ app/src/main/java/es/mresti/xemio/app/view/activity/HistoryActivity.java
@@ -0,0 +1,54 @@
+package es.mresti.xemio.app.view.activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import es.mresti.xemio.R;
+
+public class HistoryActivity extends BaseActivity {
+
+  @InjectView(R.id.toolbar) Toolbar mToolbar;
+
+  public static Intent getCallingIntent(Context context) {
+    return new Intent(context, HistoryActivity.class);
+  }
+
+  @Override public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_history);
+
+    ButterKnife.inject(this);
+    this.initialize();
+  }
+
+  /**
+   * Initializes activity's private members.
+   */
+  private void initialize() {
+    setSupportActionBar(mToolbar);
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+  }
+
+  @Override public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    //getMenuInflater().inflate(R.menu.menu_incidence, menu);
+    return true;
+  }
+
+  @Override public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    if (id == android.R.id.home) {
+      finish();
+    }
+    return super.onOptionsItemSelected(item);
+  }
+}
