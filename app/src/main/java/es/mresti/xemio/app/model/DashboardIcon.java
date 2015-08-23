e6ab4c3a522e45cc24ef22bb5f339f0b3af1f983
Esteban Dorado Roldan <fadoro@gmail.com>
2015-08-23 23:18:38 +0200

    Added DashboardIcon class

---
+++ app/src/main/java/es/mresti/xemio/app/model/DashboardIcon.java
@@ -0,0 +1,15 @@
+package es.mresti.xemio.app.model;
+
+
+public class DashboardIcon {
+  final String text;
+  final int imgId;
+  final String map;
+
+  public DashboardIcon(int imgId, String text, String map) {
+    super();
+    this.imgId = imgId;
+    this.text = text;
+    this.map = map;
+  }
+}
