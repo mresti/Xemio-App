8eaad6e7a2508f63b124149bca0597cad6254df6
Esteban Dorado Roldan <fadoro@gmail.com>
2015-10-12 20:49:29 +0200

    Added leak canary lib

--- app/src/main/java/es/mresti/xemio/app/MyApplication.java
+++ app/src/main/java/es/mresti/xemio/app/MyApplication.java
@@ -2,11 +2,13 @@ package es.mresti.xemio.app;
 
 import android.app.Application;
 import com.firebase.client.Firebase;
+import com.squareup.leakcanary.LeakCanary;
 
 public class MyApplication extends Application {
   @Override public void onCreate() {
     super.onCreate();
     Firebase.setAndroidContext(this);
     Firebase.getDefaultConfig().setPersistenceEnabled(true);
+    LeakCanary.install(this);
   }
 }
